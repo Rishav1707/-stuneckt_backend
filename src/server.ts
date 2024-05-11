2df6057876294d82e4c6fa5fffc45c29435c501d
Rishav Raj <rishavraj1707music@gmail.com>
2024-05-11 16:29:03 +0530

    feat: Add deployed frontend url to the cors policy

--- src/server.ts
+++ src/server.ts
@@ -11,7 +11,7 @@ const app: Express = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://stuneckt-frontend.vercel.app"],
   })
 );
 app.use(express.json());
